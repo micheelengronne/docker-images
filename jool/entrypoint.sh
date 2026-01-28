be9e81ad9a92d13c5bf4ecd2e53aa27126f696e8
Michée Lengronne <michee.lengronne@coppint.com>
2026-01-28 18:13:24 +0100

    feat(siit): Add siit support

--- jool/entrypoint.sh
+++ jool/entrypoint.sh
@@ -12,12 +12,22 @@ echo 'jool kernel module is compiled'
 modprobe jool
 echo 'jool kernel module is enabled'
 
-# Reattach the instance
-if [[ $(/usr/bin/jool instance display) == *${JOOL_INSTANCE:-default}* ]];
+# Reattach the nat64 instance
+if [[ $(/usr/bin/jool instance display) == *${JOOL_INSTANCE:-defaultnat64}* ]];
 then
-    /usr/bin/jool instance remove ${JOOL_INSTANCE:-default}
+    /usr/bin/jool instance remove ${JOOL_INSTANCE:-defaultnat64}
 fi
 
-/usr/bin/jool instance add ${JOOL_INSTANCE:-default} --netfilter --pool6 64:ff9b::/96;
+/usr/bin/jool instance add ${JOOL_INSTANCE:-defaultnat64} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
 
-echo "jool ${JOOL_INSTANCE:-default} instance configured"
+echo "jool ${JOOL_INSTANCE:-defaultnat64} instance configured"
+
+# Reattach the nat46 instance
+if [[ $(/usr/bin/jool_siit instance display) == *${JOOL_INSTANCE_SIIT:-defaultnat46}* ]];
+then
+    /usr/bin/jool_siit instance remove ${JOOL_INSTANCE_SIIT:-defaultnat46}
+fi
+
+/usr/bin/jool_siit instance add ${JOOL_INSTANCE_SIIT:-defaultnat46} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
+
+echo "jool ${JOOL_INSTANCE_SIIT:-defaultnat46} instance configured"
