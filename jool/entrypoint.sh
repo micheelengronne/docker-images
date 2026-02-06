1374ee970dba51b6e1a340341b53c82cc80c148d
Michée Lengronne <michee.lengronne@coppint.com>
2026-02-06 18:06:53 +0100

    feat(jool): prometheus exporter update

--- jool/entrypoint.sh
+++ jool/entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -Eeuo pipefail
 
-rmmod jool_common || true
-rmmod jool || true
-rmmod jool_siit || true
-
 apt-get update
 apt-get install -y linux-headers-$(uname -r) --no-install-recommends
 echo 'jool and jool_siit kernel modules are compiled'
@@ -14,22 +10,20 @@ echo 'jool kernel module is enabled'
 modprobe jool_siit
 echo 'jool_siit kernel module is enabled'
 
-# Reattach the nat64 instance
+# Attach the nat64 instance
 if [[ $(/usr/bin/jool instance display) == *${JOOL_INSTANCE:-defaultnat64}* ]];
 then
-    /usr/bin/jool instance remove ${JOOL_INSTANCE:-defaultnat64}
+    echo "jool instance ${JOOL_INSTANCE:-defaultnat64} already exists"
+else
+    /usr/bin/jool instance add ${JOOL_INSTANCE:-defaultnat64} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
+    echo "jool ${JOOL_INSTANCE:-defaultnat64} instance configured"
 fi
 
-/usr/bin/jool instance add ${JOOL_INSTANCE:-defaultnat64} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
-
-echo "jool ${JOOL_INSTANCE:-defaultnat64} instance configured"
-
-# Reattach the nat46 instance
+# Attach the siit instance
 if [[ $(/usr/bin/jool_siit instance display) == *${JOOL_INSTANCE_SIIT:-defaultnat46}* ]];
 then
-    /usr/bin/jool_siit instance remove ${JOOL_INSTANCE_SIIT:-defaultnat46}
+    echo "jool_siit instance ${JOOL_INSTANCE_SIIT:-defaultnat46} already exists"
+else
+    /usr/bin/jool_siit instance add ${JOOL_INSTANCE_SIIT:-defaultnat46} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
+    echo "jool ${JOOL_INSTANCE_SIIT:-defaultnat46} instance configured"
 fi
-
-/usr/bin/jool_siit instance add ${JOOL_INSTANCE_SIIT:-defaultnat46} --netfilter --pool6 ${JOOL_POOL6:-'64:ff9b::/96'}
-
-echo "jool ${JOOL_INSTANCE_SIIT:-defaultnat46} instance configured"
