98285d3589de0bed558fbb5da23cf395ca8bb8d5
Michée Lengronne <michee.lengronne@coppint.com>
2026-02-07 19:39:23 +0100

    feat(jool): make jool installable across OSes

--- jool/entrypoint.sh
+++ jool/entrypoint.sh
@@ -2,17 +2,15 @@
 
 set -Eeuo pipefail
 
-apt-get update
-arch=$(uname -m)
+cd /usr/local/src/build/jool
+./configure --disable-iptables
+make
+cp src/mod/common/*.ko /lib/modules/$(uname -r)/extra/jool/
+cp src/mod/siit/*.ko /lib/modules/$(uname -r)/extra/jool/
+cp src/mod/nat64/*.ko /lib/modules/$(uname -r)/extra/jool/
 
-if [ "$arch" = "x86_64" ]; then
-    apt-get install -y linux-headers-amd64 --no-install-recommends
-elif [ "$arch" = "aarch64" ]; then
-    apt-get install -y linux-headers-arm64 --no-install-recommends
-else
-    echo "other architecture not supported: $(uname -m)"
-    exit 250
-fi
+depmod -a
+
 echo 'jool and jool_siit kernel modules are compiled'
 modprobe jool
 echo 'jool kernel module is enabled'
