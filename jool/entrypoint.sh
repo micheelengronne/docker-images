71d60072180b684831f18438ce77390f31de14ef
Michée Lengronne <michee.lengronne@coppint.com>
2026-02-07 21:45:11 +0100

    fix(jool): modules

--- jool/entrypoint.sh
+++ jool/entrypoint.sh
@@ -2,12 +2,15 @@
 
 set -Eeuo pipefail
 
-cd /usr/local/src/build/jool
-./configure
-make
-cp src/mod/common/*.ko /lib/modules/$(uname -r)/extra/jool/
-cp src/mod/siit/*.ko /lib/modules/$(uname -r)/extra/jool/
-cp src/mod/nat64/*.ko /lib/modules/$(uname -r)/extra/jool/
+echo "This script expects that the kernel-headers from the host are mounted in /usr/src"
+ln -s /usr/src/linux-headers-$(uname -r)  /lib/modules/$(uname -r)/build
+
+cd /usr/local/src/build
+dkms build jool
+
+cp /var/lib/dkms/jool/$JOOL_VERSION/$(uname -r)/$(uname -m)/module/jool_common.ko.zst /lib/modules/$(uname -r)/extra/jool/
+cp /var/lib/dkms/jool/$JOOL_VERSION/$(uname -r)/$(uname -m)/module/jool.ko.zst /lib/modules/$(uname -r)/extra/jool/
+cp /var/lib/dkms/jool/$JOOL_VERSION/$(uname -r)/$(uname -m)/module/jool_siit.ko.zst /lib/modules/$(uname -r)/extra/jool/
 
 depmod -a
 
