7eea129fe9c2bea6f9e36b18a56443b14afaeb6d
Michée Lengronne <michee.lengronne@coppint.com>
2026-02-07 18:17:48 +0100

    fix(jool): make more os compatible

--- jool/entrypoint.sh
+++ jool/entrypoint.sh
@@ -3,7 +3,16 @@
 set -Eeuo pipefail
 
 apt-get update
-apt-get install -y linux-headers-$(uname -r) --no-install-recommends
+arch=$(uname -m)
+
+if [ "$arch" = "x86_64" ]; then
+    apt-get install -y linux-headers-amd64 --no-install-recommends
+elif [ "$arch" = "aarch64" ]; then
+    apt-get install -y linux-headers-arm64 --no-install-recommends
+else
+    echo "other architecture not supported: $(uname -m)"
+    exit 250
+fi
 echo 'jool and jool_siit kernel modules are compiled'
 modprobe jool
 echo 'jool kernel module is enabled'
